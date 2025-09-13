8fb8a8cfcb2a099eb6b1927959405074654a0745
sammy <sammedsp171@proton.me>
2025-09-13 22:33:10 +0530

    test(step 2): add test cases for step 2

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -19,3 +19,23 @@ test('should return 6 when input is "1,5"', () => {
 test('should return 5 when input is "2,3"', () => {
 	expect(add('2,3')).toBe(5)
 })
+
+test('should return 6 when input is "1,2,3"', () => {
+	expect(add('1,2,3')).toBe(6)
+})
+
+test('should return 9 when input is "2,3,4"', () => {
+	expect(add('2,3,4')).toBe(9)
+})
+
+test('should return 10 when input is "2,3,5"', () => {
+	expect(add('2,3,5')).toBe(10)
+})
+
+test('should return 18 when input is "2,3,6,7"', () => {
+	expect(add('2,3,6,7')).toBe(18)
+})
+
+test('should return 96 when input is "2,3,6,7,10,23,45"', () => {
+	expect(add('2,3,6,7,10,23,45')).toBe(96)
+})
